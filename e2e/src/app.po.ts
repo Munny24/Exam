ca044fffa6b97794ccc42b2ffdeb8f10cf3e7860
JaswanthiVellanki <73730540+Munny24@users.noreply.github.com>
2020-11-05 20:05:32 +0530

    Update app.po.ts

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,14 @@
 import { browser, by, element } from 'protractor';
 
-export class AppPage {
-  navigateTo() {
+export class AppPage 
+{
+  navigateTo()
+  {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
+  getTitleText()
+  {
     return element(by.css('app-root .content span')).getText() as Promise<string>;
   }
 }
